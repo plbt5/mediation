7774f79a9d3ed7c7b43cc826ffa17bce6bedaa7d
jeroen Bruijning <jeroen.bruijning@tno.nl>
2015-09-28 15:13:46 +0200

    Modified the method <b>eval(List<NodeValue> args)</b>: Changed instanceof test to test for FunctionBase2 expression. This change made it to pass the JUnit test.

--- src/uk/soton/service/mediation/algebra/FunctionWrapper.java
+++ src/uk/soton/service/mediation/algebra/FunctionWrapper.java
@@ -5,11 +5,13 @@ import java.util.List;
 import com.hp.hpl.jena.sparql.expr.Expr;
 import com.hp.hpl.jena.sparql.expr.ExprFunction2;
 import com.hp.hpl.jena.sparql.expr.NodeValue;
-import com.hp.hpl.jena.sparql.expr.ExprFunction1; 
+import com.hp.hpl.jena.sparql.expr.ExprFunction1;
 import com.hp.hpl.jena.sparql.function.FunctionBase2;
 
 /**
- * The FunctionWrapper class wraps an instance of ExprFunction or FunctionBase and implement the Function interface over them.
+ * The FunctionWrapper class wraps an instance of ExprFunction or FunctionBase
+ * and implement the Function interface over them.
+ * 
  * @see com.hp.hpl.jena.sparql.expr.ExprFunction2
  * @see com.hp.hpl.jena.sparql.expr.ExprFunction1
  * @see com.hp.hpl.jena.sparql.function.FunctionBase2
@@ -18,43 +20,49 @@ import com.hp.hpl.jena.sparql.function.FunctionBase2;
 public class FunctionWrapper implements Function {
 
 	protected Object inner;
-	
-	private FunctionWrapper(Object i){
+
+	private FunctionWrapper(Object i) {
 		this.inner = i;
 	}
-	
+
 	/**
 	 * The $ method is a constructor wrapper
-	 * @param i object to wrap
+	 * 
+	 * @param i
+	 *            object to wrap
 	 * @return the instance
 	 * 
 	 */
-	public static FunctionWrapper $(Object i){
+	public static FunctionWrapper $(Object i) {
 		return new FunctionWrapper(i);
 	}
-	
+
 	@Override
 	public NodeValue eval(List<NodeValue> args) {
-		if ((this.inner instanceof ExprFunction1) && (args.size() == 1)){
-			return ((ExprFunction1)this.inner).eval(args.get(0));
-		} else if ((this.inner instanceof ExprFunction2) && (args.size() == 2)){
-			return ((ExprFunction2)this.inner).eval(args.get(0),args.get(1));
-		} else if ((this.inner instanceof ExprFunction2) && (args.size() == 2)){
-			return ((FunctionBase2)this.inner).exec(args.get(0),args.get(1));
-		} else return null;
+		if ((this.inner instanceof ExprFunction1) && (args.size() == 1)) {
+			return ((ExprFunction1) this.inner).eval(args.get(0));
+		} else if ((this.inner instanceof ExprFunction2) && (args.size() == 2)) {
+			return ((ExprFunction2) this.inner).eval(args.get(0), args.get(1));
+			
+	 // } else if ((this.inner instanceof ExprFunction2) && (args.size() == 2)) { // Line changed by JB to:
+		} else if ((this.inner instanceof FunctionBase2) && (args.size() == 2)) { // end of change
+			return ((FunctionBase2) this.inner).exec(args.get(0), args.get(1));
+		} else
+			return null;
 	}
 
 	@Override
 	public Expr copy(List<Expr> args) {
-		if ((this.inner instanceof ExprFunction1) && (args.size() == 1)){
-			return ((ExprFunction1)this.inner).copy(args.get(0));
-		} else if ((this.inner instanceof ExprFunction2) && (args.size() == 2)){
-			return ((ExprFunction2)this.inner).copy(args.get(0),args.get(1));
-		} else return null;
+		if ((this.inner instanceof ExprFunction1) && (args.size() == 1)) {
+			return ((ExprFunction1) this.inner).copy(args.get(0));
+		} else if ((this.inner instanceof ExprFunction2) && (args.size() == 2)) {
+			return ((ExprFunction2) this.inner).copy(args.get(0), args.get(1));
+		} else
+			return null;
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		return this.inner.getClass().toString();
 	}
 
